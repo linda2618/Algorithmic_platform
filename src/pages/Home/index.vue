5e83338e76c0afac035781b7e428b7331be80812
东村的小香猪 <2124495707@qq.com>
2022-11-19 20:53:50 +0800

    搭建静态页面

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+        <h2>Welcome Home</h2>
+        <div>
+            <button>登录</button>
+            <button>注册</button>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/Login/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="login_box">
+            <h2 class="title">算法在线</h2>
+            <el-input v-model="input" placeholder="账号名" clearable />
+            <el-input v-model="input" type="password" placeholder="请输入密码" show-password />
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+body {
+    position: relative;
+}
+
+.login_box {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 480px;
+    height: 680px;
+    background-color: pink;
+    border-radius: 20px;
+}
+
+.title {
+    margin-top: 40px;
+}
+</style>
